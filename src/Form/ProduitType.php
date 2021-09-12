c25c3958c0523a609c58bbed277dc77e30a42fa1
emilieturmine <emilie.turmine@gmail.com>
2021-09-12 15:36:23 +0200

    EvalSymfony 3 -mise en place du Crud -Passage anglais->francais -mise en place lien Jarditou/accueuil et produit ds la navbar de base.html.twig

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('reference')
+            ->add('libelle')
+            ->add('description')
+            ->add('prix')
+            ->add('stock')
+            ->add('couleur')
+            ->add('Photo')
+            ->add('Categorie')
+            ->add('DateAjout')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
